13d800ed10f26932cc91197803f2dd88875420bf
bjl0615 <bjl0615@gmail.com>
2023-12-12 14:34:52 +0900

    Item_Domain

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -1,23 +1,20 @@
 package hello.itemservice.domain;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-public class item {
+public class Item {
 
     private Long id;
     private String itemName;
     private Integer price;
     private Integer quantity;
 
-    public item() {
+    public Item() {
 
     }
 
-    public item(Long id, String itemName, Integer price, Integer quantity) {
-        this.id = id;
+    public Item( String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
